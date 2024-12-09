064093b0426bc4cca68575cdc7acc20e055ab9b6
reelkrey <reelkrey@email.com>
2024-12-09 18:18:08 +0000

    add changes

--- src/ui/todo-item.ts
+++ src/ui/todo-item.ts
@@ -1,10 +1,21 @@
 import { html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 
 @customElement("todo-item")
 export class TodoItem extends LitElement {
+  @property() text = "";
+  @property({ type: Boolean }) completed = false;
+  @property({ type: Number }) taskId = 0;
+
   render() {
-    return html` <li></li> `;
+    return html`
+      <li>
+        <input type="checkbox" />
+        <span>text</span>
+        <button>edit</button>
+        <button>delete</button>
+      </li>
+    `;
   }
 }
 
