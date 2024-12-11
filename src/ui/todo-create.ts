28e7fdae3b8293d353703c6d0709f2628e57fc5f
reelkrey <reelkrey@email.com>
2024-12-11 20:02:21 +0000

    add changes

--- src/ui/todo-create.ts
+++ src/ui/todo-create.ts
@@ -3,12 +3,16 @@ import { customElement, property } from "lit/decorators.js";
 
 @customElement("todo-create")
 export class TodoCreate extends LitElement {
-  @property() text = "";
+  @property() task = "";
+
+  private handleInput(event: InputEvent) {
+    this.task = (event.target as HTMLInputElement).value;
+  }
 
   render() {
     return html`
       <div class="todo-create">
-        <input></input>
+        <input placeholder="create new task" @input=${this.handleInput} />
         <button>create</button>
       </div>
     `;
