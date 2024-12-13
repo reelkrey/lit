b2e9ddaacb77505c599b52de7a959e3a08151611
reelkrey <reelkrey@email.com>
2024-12-13 13:16:52 +0000

    add changes

--- src/ui/todo-item.ts
+++ src/ui/todo-item.ts
@@ -1,11 +1,14 @@
 import { css, html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { EventWithTarget } from "./types";
 
 @customElement("todo-item")
 export class TodoItem extends LitElement {
   @property() title = "";
+  @property() newTitle = "";
   @property({ type: Boolean }) completed = false;
   @property({ type: Number }) taskId = 0;
+  @property({ type: Boolean }) isEditing = false;
 
   private changeTaskStatus() {
     const changeTaskStatus = new CustomEvent("change-task-status", {
@@ -27,14 +30,26 @@ export class TodoItem extends LitElement {
     this.dispatchEvent(deleteTask);
   }
 
-  private editTask() {
-    const editTask = new CustomEvent("edit-task", {
-      detail: { taskId: this.taskId },
-      bubbles: true,
-      composed: true,
-    });
+  private handleTitleChange(
+    event: EventWithTarget<HTMLInputElement, InputEvent>,
+  ) {
+    this.title = event.target.value;
+  }
 
-    this.dispatchEvent(editTask);
+  private saveTitleChanges() {
+    this.isEditing = false;
+  }
+
+  private editTask() {
+    this.isEditing = true;
+
+    // const editTask = new CustomEvent("edit-task", {
+    //   detail: { taskId: this.taskId },
+    //   bubbles: true,
+    //   composed: true,
+    // });
+
+    // this.dispatchEvent(editTask);
   }
 
   render() {
@@ -48,11 +63,30 @@ export class TodoItem extends LitElement {
         <span>${this.title}</span>
         <button @click=${this.editTask}>edit</button>
         <button @click=${this.deleteTask}>delete</button>
+        ${this.isEditing
+          ? html` <div class="todo-item__edit-inner">
+              <input
+                @change=${this.handleTitleChange}
+                placeholder="edit task title"
+              />
+              <button @click=${this.saveTitleChanges}>edit</button>
+            </div>`
+          : null}
       </li>
     `;
   }
 
   static styles = css`
+    .todo-item {
+      display: flex;
+      position: relative;
+      gap: 10px;
+      align-items: center;
+      padding: 10px 15px;
+      border: 1px solid #ddd;
+      border-radius: 8px;
+    }
+
     .todo-item button {
       padding: 5px 10px;
       background-color: #007bff;
@@ -66,13 +100,16 @@ export class TodoItem extends LitElement {
       background-color: #0056b3;
     }
 
-    .todo-item {
-      display: flex;
-      gap: 10px;
-      align-items: center;
-      padding: 10px 15px;
-      border: 1px solid #ddd;
+    .todo-item__edit-inner {
+      position: absolute;
       border-radius: 8px;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+      background-color: #007bff;
     }
   `;
 }
